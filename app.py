641b41018ed8e04affdbe5ba261164a588f72f5e
Rob Hudson <robhudson@mozilla.com>
2021-10-05 16:34:56 -0700

    Dockerize app and add Makefile
    The Makefile can run either the app for local dev (`make run`) or freeze
    the app for static site building (`make freeze`).

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config['FREEZER_DESTINATION_IGNORE'] = ['*.css', '*.js', 'static/']
-app.config['FREEZER_DESTINATION'] = './build'
-app.config['FREEZER_STATIC_IGNORE'] = ['*.scss', '*.js', 'static/']
+app.config["FREEZER_DESTINATION_IGNORE"] = ["*.css", "*.js", "static/"]
+app.config["FREEZER_DESTINATION"] = "./build"
+app.config["FREEZER_STATIC_IGNORE"] = ["*.scss", "*.js", "static/"]
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/features")
+def features():
+    return render_template("features.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
